cc51d781dabbf46c969cfe1c53a6c5f240e878d5
‘StijnKA’ <stijn.willekens@outlook.com>
2024-10-17 16:00:57 +0200

    upload test H12 1 niv

---
+++ Test-H12-groepsniv-1-NIV-OPG/Test-H12-groepsniv-1-NIV-OPG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_H12_groepsniv_1_NIV_OPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * NAAM => .............
+             */
+
+            // Consolekleuren instellen.
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+
+
+
+            // Wachten op enter.
+            Console.WriteLine();
+            Console.WriteLine("Druk op enter om te eindigen.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test-H12-groepsniv-1-NIV-OPG/Test-H12-groepsniv-1-NIV-OPG/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-H12-groepsniv-1-NIV-OPG")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-H12-groepsniv-1-NIV-OPG")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("facd6c89-efe2-4fa9-93f3-1aa6d9525371")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
